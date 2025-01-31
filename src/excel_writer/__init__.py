837a10864f6bc8eb502531692a261268ae878d28
DongHunKim <ddong3525@gmail.com>
2025-01-31 17:29:23 +0900

    fix: typo in importing class name in __init__

--- src/excel_writer/__init__.py
+++ src/excel_writer/__init__.py
@@ -1,4 +1,4 @@
 from excel_writer.excel_writer import ExcelWriter
-from excel_writer.structs import Line, Align, VAlign, Border, Format, Cell, Column, Table, Sheet
+from excel_writer.excel import Line, Align, VAlign, Border, Format, Cell, Column, Table, Sheet
 
 __all__ = ["ExcelWriter", "Line", "Align", "VAlign", "Border", "Format", "Cell", "Column", "Table", "Sheet"]
